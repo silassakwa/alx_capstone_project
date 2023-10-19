b491be1f201050837e782e40a94ad4406de02c92
silassakwa <silassakwa2016@gmail.com>
2023-10-19 18:16:54 +0300

    final project

---
+++ main.js
@@ -0,0 +1,49 @@
+function validateForm(event) {
+    var task = document.getElementById("task_name").value;
+    var startdate = document.getElementById("start_date").value;
+    var enddate = document.getElementById("end_date").value;
+    var isComplete = document.getElementById("is_complete");
+    var errors = document.getElementById("error");
+
+    if (task === "") {
+        errors.textContent = "Please fill the task name";
+        event.preventDefault();
+        return false;
+    }
+    if (startdate === "") {
+        errors.textContent = "Please fill the start date";
+        event.preventDefault();
+        return false;
+    }
+    if (enddate === "") {
+        errors.textContent = "Please fill the end date";
+        event.preventDefault();
+        return false;
+    }
+    errors.textContent = "";
+
+    // Add the task to the task list
+    addTaskToList(task, startdate, enddate, isComplete.checked);
+
+    // Clear input fields
+    document.getElementById("task_name").value = "";
+    document.getElementById("start_date").value = "";
+    document.getElementById("end_date").value = "";
+    isComplete.checked = false;
+
+    event.preventDefault();
+    return true;
+}
+
+function addTaskToList(taskName, startDate, endDate, isComplete) {
+    var taskList = document.getElementById("task-list");
+    var taskItem = document.createElement("li");
+    taskItem.className = "task-item" + (isComplete ? " completed" : "");
+    taskItem.innerHTML = `
+        <span>Task: ${taskName}</span><br>
+        <span>Start Date: ${startDate}</span><br>
+        <span>End Date: ${endDate}</span><br>
+        <span>${isComplete ? "Is Complete" : "Not Complete"}</span>
+    `;
+    taskList.appendChild(taskItem);
+}

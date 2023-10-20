e4aed73742fca6e2e12c19a2b8406e0f93cde351
silassakwa <silassakwa2016@gmail.com>
2023-10-20 12:25:47 +0300

    added m ark as complete

--- main.js
+++ main.js
@@ -44,36 +44,22 @@ function addTaskToList(taskName, startDate, endDate, isComplete) {
         <span>Start Date: ${startDate}</span><br>
         <span>End Date: ${endDate}</span><br>
         <span>${isComplete ? "Is Complete" : "Not Complete"}</span>
+        <button class="complete-button" onclick="markComplete(this)">Mark as Complete</button>
+        <button class="complete-button" onclick="markNotComplete(this)">Not Complete</button>
     `;
-
-    // Create an "Edit" button
-    var editButton = document.createElement("button");
-    editButton.textContent = "Edit";
-    editButton.className = "edit-button";
-    editButton.addEventListener("click", function() {
-        // Populate the form with the task details for editing
-        document.getElementById("task_name").value = taskName;
-        document.getElementById("start_date").value = startDate;
-        document.getElementById("end_date").value = endDate;
-        document.getElementById("is_complete").checked = isComplete;
-
-        // Remove the task item from the task list
-        taskList.removeChild(taskItem);
-    });
-
-    // Create a "Delete" button
-    var deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.className = "delete-button";
-    deleteButton.addEventListener("click", function() {
-        // Remove the task item from the task list
-        taskList.removeChild(taskItem);
-    });
-
-    // Append the "Edit" and "Delete" buttons to the task item
-    taskItem.appendChild(editButton);
-    taskItem.appendChild(deleteButton);
 
     // Append the task item to the task list
     taskList.appendChild(taskItem);
+}
+
+function markComplete(button) {
+    var taskItem = button.parentNode;
+    taskItem.classList.add("completed");
+    taskItem.querySelector("span:last-child").textContent = "Is Complete";
+}
+
+function markNotComplete(button) {
+    var taskItem = button.parentNode;
+    taskItem.classList.remove("completed");
+    taskItem.querySelector("span:last-child").textContent = "Not Complete";
 }

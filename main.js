bed229a4431a13f5b4f9566f19d36fd9dbef9f91
silassakwa <silassakwa2016@gmail.com>
2023-10-20 11:58:33 +0300

    update delete functionality

--- main.js
+++ main.js
@@ -45,5 +45,35 @@ function addTaskToList(taskName, startDate, endDate, isComplete) {
         <span>End Date: ${endDate}</span><br>
         <span>${isComplete ? "Is Complete" : "Not Complete"}</span>
     `;
+
+    // Create an "Edit" button
+    var editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.className = "edit-button";
+    editButton.addEventListener("click", function() {
+        // Populate the form with the task details for editing
+        document.getElementById("task_name").value = taskName;
+        document.getElementById("start_date").value = startDate;
+        document.getElementById("end_date").value = endDate;
+        document.getElementById("is_complete").checked = isComplete;
+
+        // Remove the task item from the task list
+        taskList.removeChild(taskItem);
+    });
+
+    // Create a "Delete" button
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "delete-button";
+    deleteButton.addEventListener("click", function() {
+        // Remove the task item from the task list
+        taskList.removeChild(taskItem);
+    });
+
+    // Append the "Edit" and "Delete" buttons to the task item
+    taskItem.appendChild(editButton);
+    taskItem.appendChild(deleteButton);
+
+    // Append the task item to the task list
     taskList.appendChild(taskItem);
 }
